fc81b9da5ec3e5cfd3866acc0964509f01440f21
Dawid Balikowski <balikowskid@gmail.com>
2026-06-02 16:01:22 +0200

    Dodano navbar i szkielet UI

---
+++ server.R
@@ -0,0 +1,7 @@
+source("R/preprocess.R")
+source("R/model_spec.R")
+source("R/server_helpers.R")
+
+server <- function(input, output, session) {
+
+}
---
+++ ui.R
@@ -0,0 +1,15 @@
+source("R/ui_components.R")
+
+ui <- navbarPage(
+  title = "DBPG-LAB",
+
+  tabPanel("Dane", p("// TODO")),
+
+  tabPanel("Regresja", p("// TODO")),
+
+  tabPanel("Klasyfikacja", p("// TODO")),
+
+  tabPanel("Klasteryzacja", p("// TODO")),
+
+  tabPanel("Informacje o aplikacji", p("// TODO"))
+)
